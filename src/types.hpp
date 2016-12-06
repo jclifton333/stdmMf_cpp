2f566d3f4de362b41c618a495568a6797db3497f
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-06 18:16:13 -0500

    add type defs

---
+++ src/types.hpp
@@ -0,0 +1,16 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <boost/dynamic_bitset.hpp>
+
+namespace stdmMf {
+
+
+typedef std::pair<boost::dynamic_bitset<>,
+                  boost::dynamic_bitset<> > inf_trt_pair;
+
+
+} // namespace stdmMf
+
+
+#endif // TYPES_HPP
