039d0722f52f5d2ff9dd7c5bdae542967661e542
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-07 13:00:34 -0500

    add git description

---
+++ src/git_description.hpp
@@ -0,0 +1,14 @@
+#ifndef GIT_DESCRIPTION_HPP
+#define GIT_DESCRIPTION_HPP
+
+#include <string>
+
+namespace stdmMf {
+
+
+extern const std::string git_description;
+
+
+} // namespace stdmMf
+
+#endif
