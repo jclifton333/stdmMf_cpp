6644c41e15894fe1d94639ce2827affe82fbb9c7
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-09 14:42:19 -0500

    finish proximal agent

--- src/proximalAgent.cpp
+++ src/proximalAgent.cpp
@@ -14,7 +14,28 @@ boost::dynamic_bitset<> apply_trt(const boost::dynamic_bitset<> & inf_bits,
     for (uint32_t i = 0; i < this->num_nodes_; ++i) {
         bool next_to_opp = false;
         const double draw = this->rng.runif_01();
+
+        const Node & node = this->network_.get_node(i);
+        const uint32_t num_neigh = node.neigh_size();
+        for (uint32_t j = 0; j < num_neigh; ++j) {
+            if (inf_bits.test(node.neigh(j))) {
+                next_to_opp = true;
+                break;
+            }
+        }
+
+        // negative since sorting is ascending order
+        sorted.push_back(std::pair<double, uint32_t>(- draw - next_to_opp, i));
     }
+
+    boost::dynamic_bitset<> trt_bits(this->num_nodes_);
+    // sort in ascending order
+    std::sort(sorted.begin(), sorted.end());
+    for (uint32_t i = 0; i < this->num_trt_; ++i) {
+        trt_bits.set(sorted.at(i).second);
+    }
+
+    return trt_bits;
 }
 
 } // namespace stdmMf
