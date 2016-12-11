51114b6199be56594d2fbbb3f2b4bc719c966840
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-10 20:50:28 -0500

    implement myopic agent

---
+++ src/myopicAgent.cpp
@@ -0,0 +1,51 @@
+#include "myopicAgent.hpp"
+
+#include "proximalAgent.hpp"
+
+namespace stdmMf {
+
+
+MyopicAgent::MyopicAgent(const std::shared_ptr<const Network> & network,
+        const std::shared_ptr<Model> & model)
+    : Agent(network), model_(model) {
+}
+
+
+boost::dynamic_bitset<> MyopicAgent::apply_trt(
+        const boost::dynamic_bitset<> & inf_bits,
+        const std::vector<BitsetPair> & history) {
+    boost::dynamic_bitset<> trt_bits;
+    if (history.size() > 0) {
+        // add current infections to history for model fitting
+        std::vector<BitsetPair> all_history = history;
+        all_history.push_back(BitsetPair(inf_bits, trt_bits));
+
+        // get probabilities
+        this->model_->est_par(all_history);
+        const std::vector<double> probs = this->model_->probs(inf_bits,
+                trt_bits);
+
+        // set up sorting
+        std::vector<std::pair<double, uint32_t> > sorted;
+        for (uint32_t i = 0; i < this->num_nodes_; ++i) {
+            // negative probability because of ascending order
+            sorted.push_back(std::pair<double, uint32_t>(- probs.at(i), i));
+        }
+
+        std::sort(sorted.begin(), sorted.end());
+
+        trt_bits.reset(this->num_nodes_);
+        for (uint32_t i = 0; i < this->num_trt_; ++i) {
+            trt_bits.set(sorted.at(i).second);
+        }
+    } else {
+        // not enough data to estimate a model
+        ProximalAgent pa(this->network_);
+        trt_bits = pa.apply_trt(inf_bits, history);
+    }
+    return trt_bits;
+}
+
+
+
+} // namespace stdmMf
---
+++ src/myopicAgent.hpp
@@ -0,0 +1,29 @@
+#ifndef MYOPIC_AGENT_HPP
+#define MYOPIC_AGENT_HPP
+
+#include "agent.hpp"
+#include "random.hpp"
+#include "model.hpp"
+
+namespace stdmMf {
+
+
+class MyopicAgent : public Agent, public RngClass {
+protected:
+    const std::shared_ptr<Model> model_;
+
+public:
+    MyopicAgent(const std::shared_ptr<const Network> & network,
+            const std::shared_ptr<Model> & model);
+
+    virtual boost::dynamic_bitset<> apply_trt(
+            const boost::dynamic_bitset<> & inf_bits,
+            const std::vector<BitsetPair> & history);
+
+};
+
+
+} // namespace stdmMf
+
+
+#endif // MYOPIC_AGENT_HPP
