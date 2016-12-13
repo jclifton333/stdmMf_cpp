d8a1eae5b7b276f8edf4ee294f11c7dea9d076d4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-13 15:44:13 -0500

    add more tests

--- test/test_networkRunFeatures.cpp
+++ test/test_networkRunFeatures.cpp
@@ -7,6 +7,611 @@
 namespace stdmMf {
 
 
+TEST(TestNetworkRunFeatures, TestFeaturesSimpleLen1) {
+    // generate network
+    NetworkInit init;
+    init.set_dim_x(1);
+    init.set_dim_y(1);
+    init.set_wrap(false);
+    init.set_type(NetworkInit_NetType_GRID);
+
+    std::shared_ptr<Network> n = Network::gen_network(init);
+
+    NetworkRunFeatures nrf(n, 1);
+
+    ASSERT_EQ(nrf.num_features(), 1 + 3);
+
+    boost::dynamic_bitset<> inf_bits(1);
+    boost::dynamic_bitset<> trt_bits(1);
+
+    std::vector<double> f;
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    EXPECT_EQ(f.at(0), 1);
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+
+    inf_bits.reset();
+    trt_bits.reset();
+
+    trt_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    EXPECT_EQ(f.at(0), 1);
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 0);
+
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    EXPECT_EQ(f.at(0), 1);
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 1);
+
+    inf_bits.reset();
+    trt_bits.reset();
+
+    trt_bits.set(0);
+    inf_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    EXPECT_EQ(f.at(0), 1);
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+}
+
+
+TEST(TestNetworkRunFeatures, TestFeaturesSimpleLen2) {
+    // generate network
+    NetworkInit init;
+    init.set_dim_x(1);
+    init.set_dim_y(2);
+    init.set_wrap(false);
+    init.set_type(NetworkInit_NetType_GRID);
+
+    std::shared_ptr<Network> n = Network::gen_network(init);
+
+    NetworkRunFeatures nrf(n, 2);
+
+    ASSERT_EQ(nrf.num_features(), 1 + 3 + 15);
+
+    boost::dynamic_bitset<> inf_bits(2);
+    boost::dynamic_bitset<> trt_bits(2);
+
+    std::vector<double> f;
+    // trt: (0, 0), inf: (0, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 2);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 1);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (1, 0), inf: (0, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    trt_bits.set(0);
+
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 1);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 1), inf: (0, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    trt_bits.set(1);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 1);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (1, 1), inf: (0, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    trt_bits.set();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 2);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 1);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 0), inf: (1, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 1);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (1, 0), inf: (1, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(0);
+    trt_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 1);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 1), inf: (1, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(0);
+    trt_bits.set(1);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 1);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+
+    // trt: (1, 1), inf: (1, 0)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(0);
+    trt_bits.set();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 1);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+
+    // trt: (0, 0), inf: (0, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(1);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 1);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (1, 0), inf: (0, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(1);
+    trt_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 1);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 1), inf: (0, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(1);
+    trt_bits.set(1);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 1);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 1);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (1, 1), inf: (0, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set(1);
+    trt_bits.set();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 1);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 1);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 0), inf: (1, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 2);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 1);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+
+
+    // trt: (1, 0), inf: (1, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set();
+    trt_bits.set(0);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 1);
+    EXPECT_EQ(f.at(18), 0);
+
+    // trt: (0, 1), inf: (1, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set();
+    trt_bits.set(1);
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 1);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 1);
+
+    // trt: (1, 1), inf: (1, 1)
+    inf_bits.reset();
+    trt_bits.reset();
+
+    inf_bits.set();
+    trt_bits.set();
+
+    f = nrf.get_features(inf_bits, trt_bits);
+    ASSERT_EQ(f.size(), nrf.num_features());
+
+    // intercept
+    EXPECT_EQ(f.at(0), 1);
+    // len 1
+    EXPECT_EQ(f.at(1), 0);
+    EXPECT_EQ(f.at(2), 0);
+    EXPECT_EQ(f.at(3), 0);
+    // len 2
+    EXPECT_EQ(f.at(4), 0);
+    EXPECT_EQ(f.at(5), 0);
+    EXPECT_EQ(f.at(6), 0);
+    EXPECT_EQ(f.at(7), 0);
+    EXPECT_EQ(f.at(8), 0);
+    EXPECT_EQ(f.at(9), 0);
+    EXPECT_EQ(f.at(10), 0);
+    EXPECT_EQ(f.at(11), 0);
+    EXPECT_EQ(f.at(12), 0);
+    EXPECT_EQ(f.at(13), 0);
+    EXPECT_EQ(f.at(14), 0);
+    EXPECT_EQ(f.at(15), 0);
+    EXPECT_EQ(f.at(16), 0);
+    EXPECT_EQ(f.at(17), 0);
+    EXPECT_EQ(f.at(18), 0);
+}
+
+
 TEST(TestNetworkRunFeatures, TestFeaturesLen1) {
     // generate network
     NetworkInit init;
