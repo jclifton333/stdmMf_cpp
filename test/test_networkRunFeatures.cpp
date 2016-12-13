785d5b5434dd802bc3827fdb96b0871c8df5eeef
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-13 15:44:08 -0500

    init glog

--- test/test_networkRunFeatures.cpp
+++ test/test_networkRunFeatures.cpp
@@ -264,6 +264,7 @@ TEST(TestNetworkRunFeatures, TestFeaturesLen2) {
 
 int main(int argc, char **argv)
 {
+    ::google::InitGoogleLogging(argv[0]);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
