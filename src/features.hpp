28d245dd55d737dd76ddd38edfd38a1268d99a40
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-13 14:18:20 -0500

    fix signature

--- src/features.hpp
+++ src/features.hpp
@@ -11,10 +11,13 @@ public:
             const boost::dynamic_bitset<> & inf_bits,
             const boost::dynamic_bitset<> & trt_bits) = 0;
 
-    virtual std::vector<double> update_features(
-            const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits,
-            const std::vector<double> & prev_feat) = 0;
+    virtual void update_features(
+            const uint32_t & changed_node,
+            const boost::dynamic_bitset<> & inf_bits_new,
+            const boost::dynamic_bitset<> & trt_bits_new,
+            const boost::dynamic_bitset<> & inf_bits_old,
+            const boost::dynamic_bitset<> & trt_bits_old,
+            std::vector<double> & feat) = 0;
 
     virtual uint32_t num_features() const = 0;
 };
