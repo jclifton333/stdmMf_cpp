4b8e1a80324af9ce82284f7928619092a7b1cac4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-26 19:38:13 -0500

    fix result to vec and add another for shared_ptr

--- src/result.hpp
+++ src/result.hpp
@@ -26,7 +26,20 @@ template<class T>
 std::vector<T> result_to_vec(const std::vector<Result<T> > & result) {
     std::vector<T> vec;
     std::for_each(result.begin(), result.end(),
-            [&](const Result<T> & r){vec.push_back(r.get());});
+            [&](const Result<T> & r){
+                vec.push_back(r.get());
+            });
+    return vec;
+}
+
+template<class T>
+std::vector<T> result_to_vec(
+        const std::vector<std::shared_ptr<Result<T> > > & result) {
+    std::vector<T> vec;
+    std::for_each(result.begin(), result.end(),
+            [&](const std::shared_ptr<Result<T> > & r){
+                vec.push_back(r->get());
+            });
     return vec;
 }
 
