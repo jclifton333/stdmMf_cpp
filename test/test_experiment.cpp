53315c6ccf177aac7b394afdb9ac3c3aabe8ec7d
Nicholas J Meyer <nick.j.meyer@gmail.com>
2016-12-28 21:26:00 -0500

    update tests for experiment groups

--- test/test_experiment.cpp
+++ test/test_experiment.cpp
@@ -8,74 +8,147 @@ namespace stdmMf {
 TEST(TestExperiment, IntAndDouble) {
 
     Experiment e;
-    e.add_factor(std::vector<int>({-1, 0, 1}));
-    e.add_factor(std::vector<double>({-0.1, 0.0, 0.1}));
+    Experiment::FactorGroup * g = e.add_group();
+    g->add_factor(std::vector<int>({-1, 0, 1}));
+    g->add_factor(std::vector<double>({-0.1, 0.0, 0.1}));
 
     e.start();
     Experiment::Factor f;
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, -1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, -0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.1);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 0);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, -0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 0);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.1);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, -0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.1);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, -1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.0);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.0);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 0);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.0);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 0);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.0);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.0);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.0);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, -1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.1);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 0);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 0);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.1);
 
-    CHECK(e.next());
+    ASSERT_TRUE(e.next());
     f = e.get();
-    CHECK_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
-    CHECK_EQ(f.at(0).val.int_val, 1);
-    CHECK_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
-    CHECK_EQ(f.at(1).val.double_val, 0.1);
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.1);
 
-    CHECK(!e.next());
+    ASSERT_TRUE(!e.next());
+}
+
+
+TEST(TestExperiment, MultiGroupIntAndDouble) {
+    Experiment e;
+    Experiment::FactorGroup * g0 = e.add_group();
+    g0->add_factor(std::vector<int>({-1, 1}));
+    g0->add_factor(std::vector<double>({-0.1, 0.1}));
+
+    Experiment::FactorGroup * g1 = e.add_group();
+    g1->add_factor(std::vector<int>({-2, 2}));
+    g1->add_factor(std::vector<double>({-0.2, 0.2}));
+
+    e.start();
+
+    Experiment::Factor f;
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.1);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.1);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.1);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 1);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.1);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -2);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.2);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 2);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, -0.2);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, -2);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.2);
+
+    ASSERT_TRUE(e.next());
+    f = e.get();
+    EXPECT_EQ(f.at(0).type, Experiment::FactorLevel::Type::is_int);
+    EXPECT_EQ(f.at(0).val.int_val, 2);
+    EXPECT_EQ(f.at(1).type, Experiment::FactorLevel::Type::is_double);
+    EXPECT_EQ(f.at(1).val.double_val, 0.2);
+
+    ASSERT_TRUE(!e.next());
 }
 
 
