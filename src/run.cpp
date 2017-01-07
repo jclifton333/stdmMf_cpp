55d7cc35995a4b85282fb5b4eac3dd259a2232ff
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-07 13:08:07 -0500

    run for barabasi

--- src/run.cpp
+++ src/run.cpp
@@ -28,10 +28,8 @@ int main(int argc, char *argv[]) {
     const uint32_t num_reps = 50;
 
     NetworkInit init;
-    init.set_dim_x(10);
-    init.set_dim_y(10);
-    init.set_wrap(false);
-    init.set_type(NetworkInit_NetType_GRID);
+    init.set_size(100);
+    init.set_type(NetworkInit_NetType_BARABASI);
 
     const std::shared_ptr<Network> net(Network::gen_network(init));
 
