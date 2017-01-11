3d2d11ef74b4ef91610dac5f405646489e02fe81
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-11 14:47:45 -0500

    set up results to save and run multiple settings

--- src/run.cpp
+++ src/run.cpp
@@ -19,28 +19,16 @@
 
 #include <thread>
 
+#include <fstream>
+
 using namespace stdmMf;
 
-int main(int argc, char *argv[]) {
 
-    // Pool pool(std::thread::hardware_concurrency());
-    Pool pool(std::min(50u, std::thread::hardware_concurrency()));
-
-    const uint32_t num_reps = 50;
-
-    NetworkInit init;
-    init.set_dim_x(10);
-    init.set_dim_y(10);
-    init.set_wrap(false);
-    init.set_type(NetworkInit_NetType_GRID);
-    // init.set_size(100);
-    // init.set_type(NetworkInit_NetType_BARABASI);
-
-    const std::shared_ptr<Network> net(Network::gen_network(init));
-
-    const std::shared_ptr<Model> mod(new NoCovEdgeModelMaxSo(net));
-    mod->par({-4.0, -4.0, -1.5, -8.0, 2.0, -8.0});
-    // mod->par({-4.0, -2.0, -1.5, -0.25, 0.25, -4.0});
+std::vector<std::pair<std::string, std::vector<double> > >
+run(const std::shared_ptr<Model> & mod,
+        const std::shared_ptr<Network> & net,
+        const uint32_t & num_reps) {
+    Pool pool(std::min(num_reps, std::thread::hardware_concurrency()));
 
 
     // none
@@ -288,69 +276,192 @@ int main(int argc, char *argv[]) {
 
     pool.join();
 
-    const std::pair<double, double> none_stats = mean_and_var(
-            result_to_vec(none_val));
-    std::cout << "none: "
-              << none_stats.first
-              << " (" << std::sqrt(none_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(none_time)).first
-              << " seconds"
-              << std::endl;
+    std::vector<std::pair<std::string, std::vector<double> > > all_results;
 
-    const std::pair<double, double> random_stats = mean_and_var(
-            result_to_vec(random_val));
-    std::cout << "random: "
-              << random_stats.first
-              << " (" << std::sqrt(random_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(random_time)).first
-              << " seconds"
-              << std::endl;
 
-    const std::pair<double, double> proximal_stats = mean_and_var(
-            result_to_vec(proximal_val));
-    std::cout << "proximal: "
-              << proximal_stats.first
-              << " (" << std::sqrt(proximal_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(proximal_time)).first
-              << " seconds"
-              << std::endl;
+    {
+        const std::string agent_name = "none";
+        const std::pair<double, double> none_stats = mean_and_var(
+                result_to_vec(none_val));
+        const std::vector<double> agent_res =
+            {none_stats.first,
+             std::sqrt(none_stats.second / num_reps),
+             mean_and_var(result_to_vec(none_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
 
-    const std::pair<double, double> myopic_stats = mean_and_var(
-            result_to_vec(myopic_val));
-    std::cout << "myopic: "
-              << myopic_stats.first
-              << " (" << std::sqrt(myopic_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(myopic_time)).first
-              << " seconds"
-              << std::endl;
+    {
+        const std::string agent_name = "random";
+        const std::pair<double, double> random_stats = mean_and_var(
+                result_to_vec(random_val));
+        const std::vector<double> agent_res =
+            {random_stats.first,
+             std::sqrt(random_stats.second / num_reps),
+             mean_and_var(result_to_vec(random_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
 
-    const std::pair<double, double> vfn_stats = mean_and_var(
-            result_to_vec(vfn_val));
-    std::cout << "vfn: "
-              << vfn_stats.first
-              << " (" << std::sqrt(vfn_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(vfn_time)).first
-              << " seconds"
-              << std::endl;
+    {
+        const std::string agent_name = "proximal";
+        const std::pair<double, double> proximal_stats = mean_and_var(
+                result_to_vec(proximal_val));
+        const std::vector<double> agent_res =
+            {proximal_stats.first,
+             std::sqrt(proximal_stats.second / num_reps),
+             mean_and_var(result_to_vec(proximal_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
 
-    const std::pair<double, double> br_stats = mean_and_var(
-            result_to_vec(br_val));
-    std::cout << "br: "
-              << br_stats.first
-              << " (" << std::sqrt(br_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(br_time)).first
-              << " seconds"
-              << std::endl;
+    {
+        const std::string agent_name = "myopic";
+        const std::pair<double, double> myopic_stats = mean_and_var(
+                result_to_vec(myopic_val));
+        const std::vector<double> agent_res =
+            {myopic_stats.first,
+             std::sqrt(myopic_stats.second / num_reps),
+             mean_and_var(result_to_vec(myopic_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
 
-    const std::pair<double, double> adapt_stats = mean_and_var(
-            result_to_vec(adapt_val));
-    std::cout << "adapt: "
-              << adapt_stats.first
-              << " (" << std::sqrt(adapt_stats.second / num_reps) << ")"
-              << " in " << mean_and_var(result_to_vec(adapt_time)).first
-              << " seconds"
-              << std::endl;
+    {
+        const std::string agent_name = "vfn";
+        const std::pair<double, double> vfn_stats = mean_and_var(
+                result_to_vec(vfn_val));
+        const std::vector<double> agent_res =
+            {vfn_stats.first,
+             std::sqrt(vfn_stats.second / num_reps),
+             mean_and_var(result_to_vec(vfn_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
 
+    {
+        const std::string agent_name = "br";
+        const std::pair<double, double> br_stats = mean_and_var(
+                result_to_vec(br_val));
+        const std::vector<double> agent_res =
+            {br_stats.first,
+             std::sqrt(br_stats.second / num_reps),
+             mean_and_var(result_to_vec(br_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
+
+    {
+        const std::string agent_name = "adapt";
+        const std::pair<double, double> adapt_stats = mean_and_var(
+                result_to_vec(adapt_val));
+        const std::vector<double> agent_res =
+            {adapt_stats.first,
+             std::sqrt(adapt_stats.second / num_reps),
+             mean_and_var(result_to_vec(adapt_time)).first};
+        all_results.push_back(std::pair<std::string, std::vector<double> >
+                (agent_name, agent_res));
+    }
+
+    return all_results;
+}
+
+
+int main(int argc, char *argv[]) {
+    std::vector<std::shared_ptr<Network> > networks;
+    {
+        NetworkInit init;
+        init.set_dim_x(10);
+        init.set_dim_y(10);
+        init.set_wrap(false);
+        init.set_type(NetworkInit_NetType_GRID);
+        networks.push_back(Network::gen_network(init));
+    }
+
+    {
+        NetworkInit init;
+        init.set_size(100);
+        init.set_type(NetworkInit_NetType_BARABASI);
+        networks.push_back(Network::gen_network(init));
+    }
+
+    std::vector<std::vector<double> > pars;
+    {
+        // latent infections
+        const double prob_inf_latent = 0.01;
+        const double intcp_inf_latent =
+            std::log(1. / (1. - prob_inf_latent) - 1);
+
+        // neighbor infections
+        const double prob_inf = 0.5;
+        const uint32_t prob_num_neigh = 3;
+        const double intcp_inf =
+            std::log(std::pow(1. - prob_inf, -1. / prob_num_neigh) - 1.);
+
+        const double trt_act_inf =
+            std::log(std::pow(1. - prob_inf * 0.25, -1. / prob_num_neigh) - 1.)
+            - intcp_inf;
+
+        const double trt_pre_inf =
+            std::log(std::pow(1. - prob_inf * 0.75, -1. / prob_num_neigh) - 1.)
+            - intcp_inf;
+
+        // recovery
+        const double prob_rec = 0.25;
+        const double intcp_rec = std::log(1. / (1. - prob_rec) - 1.);
+        const double trt_act_rec =
+            std::log(1. / ((1. - prob_rec) * 0.5) - 1.) - intcp_rec;
+
+
+        std::vector<double> par =
+            {intcp_inf_latent,
+             intcp_inf,
+             intcp_rec,
+             trt_act_inf,
+             trt_act_rec,
+             trt_pre_inf};
+
+        pars.push_back(par);
+    }
+
+    std::ofstream ofs;
+    ofs.open("run_results.txt", std::ios::out);
+    ofs << "network,model,mean,agent,mean,se,time" << std::endl;
+
+    for (uint32_t i = 0; i < networks.size(); ++i) {
+        for (uint32_t j = 0; j < pars.size(); ++j) {
+            const std::shared_ptr<Network> & net = networks.at(i);
+            const std::shared_ptr<Model> mod(new NoCovEdgeModelMaxSo(net));
+            mod->par(pars.at(j));
+
+            const std::vector<std::pair<std::string, std::vector<double> > >
+                results = run(mod, net, 50);
+
+            ofs.open("run_results.txt", std::ios::app);
+            CHECK(ofs.good());
+
+            std::cout << "====================================="
+                      << "results for network " << net->kind() << " and model "
+                      << j << std::endl;
+
+            for (uint32_t k = 0; k < results.size(); ++k) {
+                ofs << net->kind() << ","
+                   << j << ","
+                   << results.at(k).first << ","
+                   << results.at(k).second.at(0) << ","
+                   << results.at(k).second.at(1) << ","
+                   << results.at(k).second.at(2) << std::endl;
+
+                std::cout << results.at(k).first << ": "
+                          << results.at(k).second.at(0) << " ("
+                          << results.at(k).second.at(1) << ")  ["
+                          << results.at(k).second.at(2) << "]"
+                          << std::endl;
+            }
+
+            ofs.close();
+        }
+    }
 
     return 0;
 }
