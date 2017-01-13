449f63f9eee13482bda984092cff50dddcc922cd
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-13 09:15:43 -0500

    Revert "increase size of networks"
    This reverts commit 07e42bbf7c0a97673a8d46cb45c6c7f1b19e377e.

--- src/run.cpp
+++ src/run.cpp
@@ -369,8 +369,8 @@ int main(int argc, char *argv[]) {
     std::vector<std::shared_ptr<Network> > networks;
     {
         NetworkInit init;
-        init.set_dim_x(25);
-        init.set_dim_y(20);
+        init.set_dim_x(10);
+        init.set_dim_y(10);
         init.set_wrap(false);
         init.set_type(NetworkInit_NetType_GRID);
         networks.push_back(Network::gen_network(init));
@@ -378,7 +378,7 @@ int main(int argc, char *argv[]) {
 
     {
         NetworkInit init;
-        init.set_size(500);
+        init.set_size(100);
         init.set_type(NetworkInit_NetType_BARABASI);
         networks.push_back(Network::gen_network(init));
     }
