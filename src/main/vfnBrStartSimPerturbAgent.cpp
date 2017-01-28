0de65e595b98eb86d6271299a15ae2cbff517eb4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-01-28 17:51:56 -0500

    fix vfn with br start agent

---
+++ src/main/vfnBrStartSimPerturbAgent.cpp
@@ -0,0 +1,198 @@
+#include "vfnBrStartSimPerturbAgent.hpp"
+
+#include <glog/logging.h>
+
+#include <armadillo>
+
+#include "system.hpp"
+#include "objFns.hpp"
+
+#include "proximalAgent.hpp"
+#include "myopicAgent.hpp"
+
+namespace stdmMf {
+
+
+VfnBrStartSimPerturbAgent::VfnBrStartSimPerturbAgent(
+        const std::shared_ptr<const Network> & network,
+        const std::shared_ptr<Features> & features,
+        const std::shared_ptr<Model> & model,
+        const uint32_t & vfn_num_reps,
+        const uint32_t & vfn_final_t,
+        const double & vfn_c,
+        const double & vfn_t,
+        const double & vfn_a,
+        const double & vfn_b,
+        const double & vfn_ell,
+        const double & vfn_min_step_size,
+        const double & br_c,
+        const double & br_t,
+        const double & br_a,
+        const double & br_b,
+        const double & br_ell,
+        const double & br_min_step_size)
+    : Agent(network), features_(features), model_(model),
+
+      vfn_num_reps_(vfn_num_reps), vfn_final_t_(vfn_final_t), vfn_c_(vfn_c),
+      vfn_t_(vfn_t), vfn_a_(vfn_a), vfn_b_(vfn_b), vfn_ell_(vfn_ell),
+      vfn_min_step_size_(vfn_min_step_size),
+
+      br_c_(br_c), br_t_(br_t), br_a_(br_a), br_b_(br_b), br_ell_(br_ell),
+      br_min_step_size_(br_min_step_size) {
+}
+
+VfnBrStartSimPerturbAgent::VfnBrStartSimPerturbAgent(
+        const VfnBrStartSimPerturbAgent & other)
+    : Agent(other), features_(other.features_->clone()),
+      model_(other.model_->clone()),
+
+      vfn_num_reps_(other.vfn_num_reps_), vfn_final_t_(other.vfn_final_t_),
+      vfn_c_(other.vfn_c_), vfn_t_(other.vfn_t_), vfn_a_(other.vfn_a_),
+      vfn_b_(other.vfn_b_), vfn_ell_(other.vfn_ell_),
+      vfn_min_step_size_(other.vfn_min_step_size_),
+
+      br_c_(other.br_c_), br_t_(other.br_t_), br_a_(other.br_a_),
+      br_b_(other.br_b_), br_ell_(other.br_ell_),
+      br_min_step_size_(other.br_min_step_size_) {
+}
+
+std::shared_ptr<Agent> VfnBrStartSimPerturbAgent::clone() const {
+    return std::shared_ptr<Agent>(new VfnBrStartSimPerturbAgent(*this));
+}
+
+boost::dynamic_bitset<> VfnBrStartSimPerturbAgent::apply_trt(
+        const boost::dynamic_bitset<> & inf_bits) {
+    LOG(FATAL) << "Needs history to apply treatment.";
+}
+
+
+boost::dynamic_bitset<> VfnBrStartSimPerturbAgent::apply_trt(
+        const boost::dynamic_bitset<> & inf_bits,
+        const std::vector<BitsetPair> & history) {
+    if (history.size() < 1) {
+        ProximalAgent a(this->network_);
+        return a.apply_trt(inf_bits, history);
+    // } else if (history.size() < 2) {
+    //     MyopicAgent ma(this->network_, this->model_->clone());
+    //     return ma.apply_trt(inf_bits, history);
+    }
+
+    std::vector<BitsetPair> all_history(history);
+    all_history.push_back(BitsetPair(inf_bits,
+                    boost::dynamic_bitset<>(this->network_->size())));
+
+    // minimize bellman residual for starting values
+    std::vector<double> optim_par(this->features_->num_features(), 0.);
+    {
+
+        auto f = [&](const std::vector<double> & par, void * const data) {
+            SweepAgent a(this->network_, this->features_, par, 2, false);
+            a.set_rng(this->get_rng());
+
+            auto q_fn = [&](const boost::dynamic_bitset<> & inf_bits_t,
+                    const boost::dynamic_bitset<> & trt_bits_t) {
+                return dot_a_and_b(par,
+                        this->features_->get_features(inf_bits_t, trt_bits_t));
+            };
+
+            return bellman_residual_sq(all_history, &a, 0.9, q_fn);
+        };
+
+        SimPerturb sp(f, optim_par, NULL, this->br_c_, this->br_t_, this->br_a_,
+                this->br_b_, this->br_ell_, this->br_min_step_size_);
+        sp.set_rng(this->get_rng());
+
+        Optim::ErrorCode ec;
+        do {
+            ec = sp.step();
+        } while (ec == Optim::ErrorCode::CONTINUE);
+
+        CHECK_EQ(ec, Optim::ErrorCode::SUCCESS);
+
+        optim_par = sp.par();
+    }
+
+
+    // estimate model
+    this->model_->est_par(all_history);
+
+
+    // get information matrix and take inverse sqrt
+    std::vector<double> hess = this->model_->ll_hess(all_history);
+    mult_b_to_a(hess, -1.0 * (all_history.size() - 1));
+
+    const arma::mat hess_mat(hess.data(), this->model_->par_size(),
+            this->model_->par_size());
+    arma::mat eigvec;
+    arma::vec eigval;
+    arma::eig_sym(eigval, eigvec, hess_mat);
+    for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
+        if (eigval(i) > 0.0)
+            eigval(i) = std::sqrt(1.0 / eigval(i));
+        else
+            eigval(i) = 0.0;
+    }
+    const arma::mat var_sqrt = eigvec * arma::diagmat(eigval) * eigvec.t();
+
+    // sample new parameters
+    arma::vec std_norm(this->model_->par_size());
+    for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
+        std_norm(i) = this->rng->rnorm_01();
+        LOG_IF(FATAL, !std::isfinite(std_norm(i)));
+    }
+    const std::vector<double> par_samp(
+            add_a_and_b(this->model_->par(),
+                    arma::conv_to<std::vector<double> >::from(
+                            var_sqrt * std_norm)));
+
+    // set new parameters
+    this->model_->par(par_samp);
+
+
+    // maximize value function
+    {
+        const uint32_t num_points = this->vfn_final_t_ - history.size();
+
+        auto f = [&](const std::vector<double> & par, void * const data) {
+            SweepAgent a(this->network_, this->features_, par, 2, false);
+            a.set_rng(this->get_rng());
+            System s(this->network_, this->model_);
+            s.set_rng(this->get_rng());
+            double val = 0.0;
+            for (uint32_t i = 0; i < this->vfn_num_reps_; ++i) {
+                s.cleanse();
+                s.wipe_trt();
+                s.erase_history();
+                s.inf_bits(inf_bits);
+
+                val += runner(&s, &a, num_points, 0.9);
+            }
+            val /= this->vfn_num_reps_;
+
+            // return negative since optim minimizes functions
+            return -val;
+        };
+
+        SimPerturb sp(f, optim_par, NULL, this->vfn_c_, this->vfn_t_,
+                this->vfn_a_, this->vfn_b_, this->vfn_ell_,
+                this->vfn_min_step_size_);
+        sp.set_rng(this->get_rng());
+
+        Optim::ErrorCode ec;
+        do {
+            ec = sp.step();
+        } while (ec == Optim::ErrorCode::CONTINUE);
+
+        CHECK_EQ(ec, Optim::ErrorCode::SUCCESS);
+
+        optim_par = sp.par();
+    }
+
+
+    SweepAgent a(this->network_, this->features_, optim_par, 2, false);
+    a.set_rng(this->get_rng());
+    return a.apply_trt(inf_bits, history);
+}
+
+
+} // namespace stdmMf
---
+++ src/main/vfnBrStartSimPerturbAgent.hpp
@@ -0,0 +1,70 @@
+#ifndef VFN_BR_START_SIM_PERTURB_AGENT_HPP
+#define VFN_BR_START_SIM_PERTURB_AGENT_HPP
+
+#include <cstdint>
+#include "model.hpp"
+#include "agent.hpp"
+#include "features.hpp"
+#include "sweepAgent.hpp"
+#include "simPerturb.hpp"
+
+namespace stdmMf {
+
+
+class VfnBrStartSimPerturbAgent : public Agent, public RngClass {
+protected:
+    const std::shared_ptr<Features> features_;
+    const std::shared_ptr<Model> model_;
+
+    const uint32_t vfn_num_reps_;
+    const uint32_t vfn_final_t_;
+    const double vfn_c_;
+    const double vfn_t_;
+    const double vfn_a_;
+    const double vfn_b_;
+    const double vfn_ell_;
+    const double vfn_min_step_size_;
+
+    const double br_c_;
+    const double br_t_;
+    const double br_a_;
+    const double br_b_;
+    const double br_ell_;
+    const double br_min_step_size_;
+
+public:
+    VfnBrStartSimPerturbAgent(const std::shared_ptr<const Network> & network,
+            const std::shared_ptr<Features> & features,
+            const std::shared_ptr<Model> & model,
+            const uint32_t & vfn_num_reps,
+            const uint32_t & vfn_final_t,
+            const double & vfn_c,
+            const double & vfn_t,
+            const double & vfn_a,
+            const double & vfn_b,
+            const double & vfn_ell,
+            const double & vfn_min_step_size,
+            const double & br_c,
+            const double & br_t,
+            const double & br_a,
+            const double & br_b,
+            const double & br_ell,
+            const double & br_min_step_size);
+
+    VfnBrStartSimPerturbAgent(const VfnBrStartSimPerturbAgent & other);
+
+    virtual std::shared_ptr<Agent> clone() const;
+
+    virtual boost::dynamic_bitset<> apply_trt(
+            const boost::dynamic_bitset<> & inf_bits,
+            const std::vector<BitsetPair> & history);
+
+    virtual boost::dynamic_bitset<> apply_trt(
+            const boost::dynamic_bitset<> & inf_bits);
+};
+
+
+} // namespace stdmMf
+
+
+#endif // VFN_BR_START_SIM_PERTURB_AGENT_HPP
