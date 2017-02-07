2b198151b1e357a7c3b996ae8bcfb6eca093d53d
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-07 16:27:23 -0500

    fix test for progress

--- test/test_progress.cpp
+++ test/test_progress.cpp
@@ -10,20 +10,42 @@ namespace stdmMf {
 TEST(TestProgress, TestNoTotal) {
     std::stringstream ss;
     Progress<std::stringstream> p(&ss);
+    std::string match = "\\r    0 \\([ 0-9]{3}(.)[0-9]{2} hours\\)";
     EXPECT_TRUE(std::regex_match(ss.str(),
-                    std::regex("^(\r)    0 [(][ 0-9]{3}[.][0-9]{2} hours[)]$")));
+                    std::regex(match)));
 
     p.update();
+    match += "\\r    1 \\([ 0-9]{3}(.)[0-9]{2} hours\\)";
     EXPECT_TRUE(std::regex_match(ss.str(),
-                    std::regex("^[^]*\\r    1"
-                            " [(][ 0-9]{3}[.][0-9]{2} hours[)]$"
-                            )));
+                    std::regex(match)));
 
     p.update();
+    match += "\\r    2 \\([ 0-9]{3}(.)[0-9]{2} hours\\)";
     EXPECT_TRUE(std::regex_match(ss.str(),
-                    std::regex("^[^]*\\r    2"
-                            " [(][ 0-9]{3}[.][0-9]{2} hours[)]$")));
+                    std::regex(match)));
 }
+
+
+TEST(TestProgress, TestTotal2) {
+    std::stringstream ss;
+    Progress<std::stringstream> p(2, &ss);
+
+    const std::string hours = " \\([ 0-9]{3}(.)[0-9]{2} hours\\)";
+    std::string match = "\\r    0 /     2 \\[ {20}\\]" + hours;
+    EXPECT_TRUE(std::regex_match(ss.str(),
+                    std::regex(match)));
+
+    match += "\\r    1 /     2 \\[#{10} {10}\\]" + hours;
+    p.update();
+    EXPECT_TRUE(std::regex_match(ss.str(),
+                    std::regex(match)));
+
+    match += "\\r    2 /     2 \\[#{20}\\]" + hours;
+    p.update();
+    EXPECT_TRUE(std::regex_match(ss.str(),
+                    std::regex(match)));
+}
+
 
 
 } // namespace stdmMf
