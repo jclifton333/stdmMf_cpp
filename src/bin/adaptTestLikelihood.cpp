d64860255527a5a516c5080b9eac15f0047b9dcf
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-09 11:33:32 -0500

    start likelihood test script

---
+++ src/bin/adaptTestLikelihood.cpp
@@ -0,0 +1,16 @@
+#include "adaptTestData.pb.h"
+
+#include "system.hpp"
+
+#include "noCovEdgeModel.hpp"
+#include "noCovEdgeOrSoModel.hpp"
+#include "noCovEdgeXorSoModel.hpp"
+#include "noCovEdgeSepSoModel.hpp"
+
+#include "progress.hpp"
+
+using namespace stdmMf;
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
