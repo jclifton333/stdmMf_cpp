5252250e75484db875c3844f985f1a38b48f45ad
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-14 11:03:32 -0500

    add dummy comment

--- src/bin/adaptTestData.cpp
+++ src/bin/adaptTestData.cpp
@@ -267,7 +267,7 @@ int main(int argc, char *argv[]) {
                 ModelPair mp (std::shared_ptr<Model>(new NoCovEdgeModel(
                                         networks.at(i))),
                         std::shared_ptr<Model>(new NoCovEdgeSepSoModel(
-                                        networks.at(i))));
+                                        networks.at(i)))); // sfsd
                 mp.first->par(par);
                 mp.second->par(par_sep);
 
