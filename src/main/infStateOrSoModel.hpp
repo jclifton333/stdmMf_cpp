dc452a0800ce40aa0cc3280856631268cc0f9d41
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-16 12:31:02 -0500

    convert or spillover model

--- src/main/infStateOrSoModel.hpp
+++ src/main/infStateOrSoModel.hpp
@@ -1,17 +1,15 @@
-#ifndef NO_COV_EDGE_OR_SO_MODEL_HPP
-#define NO_COV_EDGE_OR_SO_MODEL_HPP
+#ifndef INF_STATE_OR_SO_MODEL_HPP
+#define INF_STATE_OR_SO_MODEL_HPP
 
 #include "types.hpp"
-#include "model.hpp"
 #include "network.hpp"
+#include "infStateModel.hpp"
 
 namespace stdmMf {
 
 
-class NoCovEdgeOrSoModel : public Model {
+class InfStateOrSoModel : public InfStateModel {
 private:
-    const uint32_t par_size_;
-
     double intcp_inf_latent_;
     double intcp_inf_;
     double intcp_rec_;
@@ -19,82 +17,69 @@ private:
     double trt_act_rec_;
     double trt_pre_inf_;
 
-    const std::shared_ptr<const Network> network_;
-    const uint32_t num_nodes_;
-
     double inf_b(const uint32_t & b_node, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     double a_inf_b(const uint32_t & a_node, const uint32_t & b_node,
             const bool & a_trt, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     double rec_b(const uint32_t & b_node, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     std::vector<double> inf_b_grad(const uint32_t & b_node,
             const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     std::vector<double> a_inf_b_grad(
             const uint32_t & a_node, const uint32_t & b_node,
             const bool & a_trt, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     std::vector<double> rec_b_grad(
             const uint32_t & b_node, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
 
     std::vector<double> inf_b_hess(const uint32_t & b_node,
             const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     std::vector<double> a_inf_b_hess(
             const uint32_t & a_node, const uint32_t & b_node,
             const bool & a_trt, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
     std::vector<double> rec_b_hess(
             const uint32_t & b_node, const bool & b_trt,
             const boost::dynamic_bitset<> & inf_bits,
-            const boost::dynamic_bitset<> & trt_bits) const;
+            const boost::dynamic_bitset<> & trt_bits) const override;
 
 
 public:
-    NoCovEdgeOrSoModel(const std::shared_ptr<const Network> & network);
+    InfStateOrSoModel(const std::shared_ptr<const Network> & network);
 
-    NoCovEdgeOrSoModel(const NoCovEdgeOrSoModel & other);
+    InfStateOrSoModel(const InfStateOrSoModel & other);
 
-    virtual std::shared_ptr<Model> clone() const;
+    ~InfStateOrSoModel() override = default;
 
-    virtual std::vector<double> par() const;
+    virtual std::shared_ptr<Model> clone() const override;
 
-    virtual void par(const std::vector<double> & par);
+    virtual std::vector<double> par() const override;
 
-    virtual uint32_t par_size() const;
-
-    virtual std::vector<double> probs(
-            const boost::dynamic_bitset<> & inf_status,
-            const boost::dynamic_bitset<> & trt_status) const;
-
-    virtual std::vector<double> ll_grad(
-            const std::vector<Transition> & history) const;
-
-    virtual std::vector<double> ll_hess(
-            const std::vector<Transition> & history) const;
+    virtual void par(const std::vector<double> & par) override;
 };
 
 
 } // namespace stdmMf
 
 
-#endif // NO_COV_EDGE_OR_SO_MODEL_HPP
+#endif // INF_STATE_OR_SO_MODEL_HPP
