19f0921eec3e64d08a1654e346a9dff66d5946d1
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-02-25 12:35:10 -0500

    fix a and min_step_size combinations

--- src/bin/brMinExperiment_infShieldState.cpp
+++ src/bin/brMinExperiment_infShieldState.cpp
@@ -237,11 +237,26 @@ int main(int argc, char *argv[]) {
 
         g->add_factor(std::vector<double>({0.2, 0.1, 0.05})); // c
         g->add_factor(std::vector<double>({0.05, 0.1, 0.15, 0.35})); // t
-        g->add_factor(std::vector<double>({1.41e-0, 1.41e-1})); // a
+        g->add_factor(std::vector<double>({1.41e-0})); // a
         g->add_factor(std::vector<double>({1})); // b
         g->add_factor(std::vector<double>({0.85})); // ell
         g->add_factor(std::vector<double>(
-                        {2.79e-3, 1.29e-3, 7.15e-4})); // min_step_size
+                        {2.79e-2, 1.29e-2, 7.15e-3})); // min_step_size
+        g->add_factor(std::vector<int>({1, 2})); // run_length
+        g->add_factor(std::vector<bool>({false, true})); // do_sweeps
+        g->add_factor(std::vector<bool>({false, true})); // sq_br
+    }
+
+    {
+        Experiment::FactorGroup * g = e.add_group();
+
+        g->add_factor(std::vector<double>({0.2, 0.1, 0.05})); // c
+        g->add_factor(std::vector<double>({0.05, 0.1, 0.15, 0.35})); // t
+        g->add_factor(std::vector<double>({1.41e-1})); // a
+        g->add_factor(std::vector<double>({1})); // b
+        g->add_factor(std::vector<double>({0.85})); // ell
+        g->add_factor(std::vector<double>(
+            {2.79e-3, 1.29e-3, 7.15e-4})); // min_step_size
         g->add_factor(std::vector<int>({1, 2})); // run_length
         g->add_factor(std::vector<bool>({false, true})); // do_sweeps
         g->add_factor(std::vector<bool>({false, true})); // sq_br
