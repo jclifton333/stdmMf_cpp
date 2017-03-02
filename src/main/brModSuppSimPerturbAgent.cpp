929f27c7769385504b8a3e6cb858da1fd7da81b4
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-03-02 13:44:41 -0500

    name space fix

--- src/main/brModSuppSimPerturbAgent.cpp
+++ src/main/brModSuppSimPerturbAgent.cpp
@@ -139,7 +139,7 @@ std::vector<double> BrModSuppSimPerturbAgent<State>::train(
 template <typename State>
 void BrModSuppSimPerturbAgent<State>::rng(
         const std::shared_ptr<njm::tools::Rng> & rng) {
-    this->RngClass::rng(rng);
+    this->njm::tools::RngClass::rng(rng);
     this->model_->rng(rng);
 }
 
