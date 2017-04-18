7bc400d0b375884eb996cb4f872abab1384cb030
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-04-18 10:49:12 -0400

    add clone vec function

---
+++ src/main/utilities.hpp
@@ -0,0 +1,25 @@
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include <vector>
+#include <memory>
+
+namespace stdmMf {
+
+
+template <typename T>
+std::vector<std::shared_ptr<T> > clone_vec(
+        const std::vector<std::shared_ptr<T> > & vec) {
+    std::vector<std::shared_ptr<T> > vec_2(vec.size());
+    std::transform(vec.begin(), vec.end(), vec_2.begin(),
+            [] (const std::shared_ptr<T> & t_) {
+                return t_->clone();
+            });
+    return vec_2;
+}
+
+
+} // namespace stdmMf
+
+
+#endif // UTILITIES_HPP
