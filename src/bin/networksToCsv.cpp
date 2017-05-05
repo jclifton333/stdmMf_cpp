f23b5851904f8388a6686139a5442c33157ac044
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-05-05 13:14:45 -0400

    add script to generate csv files with networks edges

---
+++ src/bin/networksToCsv.cpp
@@ -0,0 +1,90 @@
+#include "network.hpp"
+
+#include <njm_cpp/data/trapperKeeper.hpp>
+#include <njm_cpp/info/project.hpp>
+
+using namespace stdmMf;
+
+std::string edges_to_csv(const std::shared_ptr<Network> & network) {
+    std::stringstream ss;
+    ss << "index_a,index_b,x_a,y_a,x_b,y_b\n";
+    const auto edges(network->edges());
+    for (auto it = edges.begin(); it != edges.end(); ++it) {
+        const Node & node_a(network->get_node(it->first));
+        const Node & node_b(network->get_node(it->second));
+
+        ss << node_a.index() << ","
+           << node_b.index() << ","
+           << node_a.x() << ","
+           << node_a.y() << ","
+           << node_b.x() << ","
+           << node_b.y() << "\n";
+    }
+
+    return ss.str();
+}
+
+int main(int argc, char *argv[])
+{
+
+    njm::data::TrapperKeeper tk(argv[0],
+            njm::info::project::PROJECT_ROOT_DIR + "/data");
+
+    std::vector<NetworkInit> inits;
+
+    {// grid 100
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_GRID);
+        init.set_dim_x(10);
+        init.set_dim_y(10);
+        init.set_wrap(false);
+        inits.push_back(init);
+    }
+
+    {// grid 500
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_GRID);
+        init.set_dim_x(20);
+        init.set_dim_y(25);
+        init.set_wrap(false);
+        inits.push_back(init);
+    }
+
+    {// grid 1000
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_GRID);
+        init.set_dim_x(40);
+        init.set_dim_y(25);
+        init.set_wrap(false);
+        inits.push_back(init);
+    }
+
+    {// barabasi 100
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_BARABASI);
+        init.set_size(100);
+        inits.push_back(init);
+    }
+
+    {// barabasi 500
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_BARABASI);
+        init.set_size(500);
+        inits.push_back(init);
+    }
+
+    {// barabasi 1000
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_BARABASI);
+        init.set_size(1000);
+        inits.push_back(init);
+    }
+
+    // write networks to csv
+    for (auto it = inits.begin(); it != inits.end(); ++it) {
+        const auto network(Network::gen_network(*it));
+        *tk.entry(network->kind() + ".csv") << edges_to_csv(network);
+    }
+
+    return 0;
+}
