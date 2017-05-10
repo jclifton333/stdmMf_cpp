add63072054f1dee52db7e9922ece17eca0df05c
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-05-10 18:17:49 -0400

    start script to plot networks

---
+++ scripts/plotNetworks.py
@@ -0,0 +1,22 @@
+"""Plot networks for use in paper"""
+
+import os
+import sys
+
+root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..")
+sys.path.append(os.path.join(root_dir, "build/src/main/"))
+import stdmMf_pb2
+
+import igraph
+
+
+def form_adjacency():
+    pass
+
+
+def plot_network():
+    pass
+
+
+if __name__ == "__main__":
+    pass
