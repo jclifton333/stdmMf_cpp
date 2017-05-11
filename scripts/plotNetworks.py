f7499869dc84d03e4be27a36c01048824d16ac56
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-05-10 22:54:32 -0400

    script to plot networks

--- scripts/plotNetworks.py
+++ scripts/plotNetworks.py
@@ -5,18 +5,67 @@ import sys
 
 root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"..")
 sys.path.append(os.path.join(root_dir, "build/src/main/"))
-import stdmMf_pb2
+import network_pb2
 
 import igraph
 
+import argparse
 
-def form_adjacency():
-    pass
+import re
+
+def form_adjacency(node_list):
+    num_nodes = len(node_list.nodes)
+    adj = [ [0] * num_nodes for i in range(num_nodes)]
+    coords = []
+    for node in node_list.nodes:
+        if node.HasField("x") and node.HasField("y"):
+            coords.append((node.x, node.y))
+
+        for neigh in node.neigh:
+            adj[node.index][neigh] = 1
+
+    assert len(coords) == 0 or len(coords) == num_nodes
+
+    if len(coords) == 0:
+        return adj, None
+    else:
+        return adj, coords
+
+def plot_network(node_list):
+    adj, layout = form_adjacency(node_list)
+
+    graph = igraph.Graph.Adjacency(adj).as_undirected()
+
+    if layout is None:
+        igraph.plot(graph, layout = "fr",
+                    vertex_size = 4, vertex_color = "#000000")
+    else:
+        igraph.plot(graph, layout = layout,
+                    vertex_size = 4, vertex_color = "#000000")
 
 
-def plot_network():
-    pass
+def main(data_dir):
+    net_check = re.compile("(grid_[0-9]+x[0-9]+\.pb"
+                           "|barabasi_[0-9]+\.pb"
+                           "|random_[0-9]+\.pb)")
+
+    net_files = []
+    for file_name in os.listdir(data_dir):
+        if net_check.match(file_name):
+            net_files.append(file_name)
+
+
+    for net_file in net_files:
+        node_list = network_pb2.NodeList()
+        with open(os.path.join(data_dir, net_file), "r") as f:
+            node_list.ParseFromString(f.read())
+        plot_network(node_list)
 
 
 if __name__ == "__main__":
-    pass
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--data-dir", type = str, required = True)
+
+    args = ap.parse_args()
+
+    main(args.data_dir)
