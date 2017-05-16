dba27a3eb28f07b1e0166428b7ee5f0c5a8465a1
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-05-16 13:53:22 -0400

    fix casting issue

--- src/main/utilities.hpp
+++ src/main/utilities.hpp
@@ -13,7 +13,7 @@ std::vector<std::shared_ptr<T> > clone_vec(
     std::vector<std::shared_ptr<T> > vec_2(vec.size());
     std::transform(vec.begin(), vec.end(), vec_2.begin(),
             [] (const std::shared_ptr<T> & t_) {
-                return t_->clone();
+                return std::static_pointer_cast<T>(t_->clone());
             });
     return vec_2;
 }
