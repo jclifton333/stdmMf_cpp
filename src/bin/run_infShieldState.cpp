a96c23a1524bffe1220663746fe82516c8dfa1ce
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-05-23 14:30:18 -0700

    set reps to 50 and time points to 25

--- src/bin/run_infShieldState.cpp
+++ src/bin/run_infShieldState.cpp
@@ -819,8 +819,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    const uint32_t num_reps = 2;
-    const uint32_t time_points = 2;
+    const uint32_t num_reps = 50;
+    const uint32_t time_points = 25;
 
     // set up results containers
     const std::vector<std::string> agent_names({
