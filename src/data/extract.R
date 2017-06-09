431488feabdb6ca61cbe979d3219bb0c6efd9dc0
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-06-09 17:33:29 -0400

    add more comments about data extraction

--- src/data/extract.R
+++ src/data/extract.R
@@ -3,35 +3,46 @@ rm(list=ls(all=TRUE))
 ## download data and code for Ebola paper from
 ## http://datadryad.org/resource/doi:10.5061/dryad.k95j3.2
 
+## directory to unziped data
 data_dir = "~/Downloads/DataAndCode/DataAndcode"
 
+
+## polygon data set
 polygons = readRDS(paste(data_dir, "WestAfricaCountyPolygons.rds", sep="/"))
 
+
+## outbreaks data set
 outbreaks = readRDS(paste(data_dir,
                           "OutbreakDateByCounty_Summer_AllCountries.rds",
                           sep="/"))
+## sort outbreaks by county
 outbreaks = outbreaks[order(as.character(outbreaks$county_names)),]
 
+
+## id data set
 ids = readRDS(paste(data_dir, "MobilityDataIDs.rds", sep="/"))
+## fix special character issues
 ids$county[which(ids$county=="Gbapolu")] = "Gbarpolu"
 ids$county[which(ids$county=="BafatÃ¡")] = "Bafatá"
 ids$county[which(ids$county=="GabÃº")] = "Gabú"
+## sort ids by county
 ids = ids[order(ids$county),]
 
+
+## read in supplemental data to get x and y coordinates
 admn = read.csv(paste(data_dir, "AdmUnits_WBtwn.csv", sep="/"), as.is=TRUE)
 
 
-## pull out necessary data
+## combine necessary data for simulations
 ebola = data.frame(county = ids$county,
                    country = ids$country,
                    loc = ids$loc,
                    outbreak = outbreaks$infection_date,
                    population = polygons$pop.size,
-                   x = 0,
-                   y = 0
+                   x = 0, y = 0 ## temporary
                    )
 
-## get x and y from admn
+## get x and y
 for(i in 1:nrow(ebola)) {
   ## extract to and from that matches ebola location code
   from_vals = admn[which(admn$from_loc == ebola$loc[i]), ]
