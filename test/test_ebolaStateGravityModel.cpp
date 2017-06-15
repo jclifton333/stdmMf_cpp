903b1b261823ca33bc557f8a82d102dee8d3277b
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-06-14 21:05:28 -0400

    update tests for ebola

--- test/test_ebolaStateGravityModel.cpp
+++ test/test_ebolaStateGravityModel.cpp
@@ -101,11 +101,11 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
     // set par
     njm::tools::Rng rng;
     std::vector<double> par(m->par());
-    par.at(0) = -5.246;
-    par.at(1) = -155.8;
-    par.at(2) = 0.186;
-    par.at(3) = -2.0;
-    par.at(4) = -1.0;
+    par.at(0) = -3.0;
+    par.at(1) = 1.0;
+    par.at(2) = 0.1;
+    par.at(3) = -3.0;
+    par.at(4) = -2.0;
     m->par(par);
 
     System<EbolaState> s(n,m);
@@ -123,7 +123,7 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
     // generate new parameters so gradient is not zero
     std::for_each(par.begin(),par.end(),
             [&rng](double & x) {
-                x += rng.runif_01();
+                x += rng.rnorm(0.0, std::abs(x) / 10.0);
             });
     m->par(par);
     const std::vector<double> grad_val =
@@ -143,6 +143,8 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
         double result;
         double abserr;
         gsl_deriv_central(&F, par.at(i), 1e-3, &result, &abserr);
+
+        std::cout << result << std::endl;
 
         EXPECT_NEAR(grad_val.at(i), result, eps)
             << "gradient failed for parameter " << i;
@@ -165,18 +167,19 @@ TEST(TestEbolaStateGravityModel,TestLLHessian) {
     // set par
     njm::tools::Rng rng;
     std::vector<double> par(m->par());
-    par.at(0) = -5.246;
-    par.at(1) = -155.8;
-    par.at(2) = 0.341;
-    par.at(3) = -2.0;
-    par.at(4) = -1.0;
+    par.at(0) = -3.0;
+    par.at(1) = 1.0;
+    par.at(2) = 0.1;
+    par.at(3) = -3.0;
+    par.at(4) = -2.0;
     m->par(par);
 
     System<EbolaState> s(n,m);
+    s.start();
 
     RandomAgent<EbolaState> a(n);
 
-    const uint32_t num_points = 3;
+    const uint32_t num_points = 10;
     runner(&s, &a, num_points, 1.0);
 
     std::vector<Transition<EbolaState>> history(
@@ -187,11 +190,12 @@ TEST(TestEbolaStateGravityModel,TestLLHessian) {
     // generate new parameters so gradient is not zero
     std::for_each(par.begin(),par.end(),
             [&rng](double & x) {
-                x += rng.rnorm_01();
+                x += rng.rnorm(0.0, std::abs(x) / 10);
             });
     m->par(par);
     const std::vector<double> hess_val =
         m->ll_hess(history);
+
 
     for (uint32_t i = 0; i < par.size(); ++i) {
         for (uint32_t j = 0; j < par.size(); ++j) {
@@ -209,8 +213,11 @@ TEST(TestEbolaStateGravityModel,TestLLHessian) {
             double result;
             double abserr;
             gsl_deriv_central(&F, par.at(i), 1e-3, &result, &abserr);
+            std::cout << result << " == "
+                      << hess_val.at(i * par.size() + j) << std::endl;
 
-            EXPECT_NEAR(hess_val.at(i * par.size() + j), result, eps)
+            const double diff(hess_val.at(i * par.size() + j) - result);
+            EXPECT_LT(std::abs(diff / result), eps)
                 << "hessian failed for parameters " << i << " and " << j;
         }
     }
@@ -229,14 +236,15 @@ TEST(TestEbolaStateGravityModel, EstPar) {
 
     njm::tools::Rng rng;
     std::vector<double> par(m->par_size());
-    par.at(0) = -5.246;
-    par.at(1) = -155.8;
-    par.at(2) = 0.341;
-    par.at(3) = 2.0;
-    par.at(4) = 2.5;
+    par.at(0) = -3.0;
+    par.at(1) = 1.0;
+    par.at(2) = 0.08;
+    par.at(3) = -1.0;
+    par.at(4) = -1.5;
     m->par(par);
 
     System<EbolaState> s(n,m);
+    s.start();
 
     const std::shared_ptr<ProximalAgent<EbolaState> > pa(
             new ProximalAgent<EbolaState>(n));
@@ -244,7 +252,7 @@ TEST(TestEbolaStateGravityModel, EstPar) {
             new RandomAgent<EbolaState>(n));
     EpsAgent<EbolaState> ea(n, pa, ra, 0.1);
 
-    const uint32_t num_points = 100;
+    const uint32_t num_points = 200;
     runner(&s, &ea, num_points, 1.0);
 
     std::vector<Transition<EbolaState> > history(
@@ -254,16 +262,18 @@ TEST(TestEbolaStateGravityModel, EstPar) {
     // scale paramters
     std::vector<double> start_par = par;
     std::for_each(start_par.begin(), start_par.end(),
-            [] (double & x) {
-                x *= 10.0;
+            [&rng] (double & x) {
+                x += rng.rnorm(0.0, std::abs(x) / 10.0);
             });
+    m->par(start_par);
 
     m->est_par(history);
 
     const std::vector<double> est_par = m->par();
     for (uint32_t i = 0; i < m->par_size(); ++i) {
-        const double diff = std::abs(par.at(i) - est_par.at(i));
-        EXPECT_LT(diff / par.at(i), 0.1)
+        const double diff(par.at(i) - est_par.at(i));
+        std::cout << par.at(i) << " == " << est_par.at(i) << std::endl;
+        EXPECT_LT(std::abs(diff / par.at(i)), 0.1)
             << "Par " << i << " failed with truth " << par.at(i)
             << " and estimate " << est_par.at(i);
     }
