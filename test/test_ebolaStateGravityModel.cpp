df66861a4f500d6e554285d664dc7a3009826bdd
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-06-18 14:38:09 -0400

    use mock networks for testing

--- test/test_ebolaStateGravityModel.cpp
+++ test/test_ebolaStateGravityModel.cpp
@@ -62,7 +62,9 @@ double f_grad (double x, void * params) {
 
 
 TEST(TestEbolaStateGravityModel, TestPar) {
-    EbolaData::init();
+    EbolaData::deinit();
+    // dummy ebola network
+    EbolaData::init({"a"}, {"1"}, {"a1"}, {1}, {0}, {1.0}, {0.0}, {0.0});
     // generate network
     NetworkInit init;
     init.set_type(NetworkInit_NetType_EBOLA);
@@ -87,7 +89,43 @@ TEST(TestEbolaStateGravityModel, TestPar) {
 
 
 TEST(TestEbolaStateGravityModel,TestLLGradient) {
-    EbolaData::init();
+    njm::tools::Rng rng;
+    {
+        // set up mock ebola network from a grid
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_GRID);
+        init.set_dim_x(10);
+        init.set_dim_y(10);
+        init.set_wrap(false);
+
+        std::shared_ptr<Network> grid = Network::gen_network(init);
+
+        std::vector<std::string> str_var(grid->size(), "blank");
+
+        std::vector<uint32_t> region(grid->size());
+        std::generate(region.begin(), region.end(),
+                [&rng] () {return (uint32_t)rng.rint(0, 10);});
+
+        std::vector<int> outbreaks(grid->size());
+        std::generate(outbreaks.begin(), outbreaks.end(),
+                [&rng] () {return rng.rint(0, 100);});
+
+        std::vector<double> population(grid->size());
+        std::generate(population.begin(), population.end(),
+                [&rng] () {return rng.runif(3.0, 5.0);});
+
+        std::vector<double> x(grid->size());
+        std::vector<double> y(grid->size());
+
+        for (uint32_t i = 0; i < grid->size(); ++i) {
+            x.at(i) = grid->get_node(i).x();
+            y.at(i) = grid->get_node(i).y();
+        }
+
+        EbolaData::deinit();
+        EbolaData::init(str_var, str_var, str_var, region,
+                outbreaks, population, x, y);
+    }
     // generate network
     NetworkInit init;
     init.set_type(NetworkInit_NetType_EBOLA);
@@ -99,11 +137,10 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
             new EbolaStateGravityModel(n));
 
     // set par
-    njm::tools::Rng rng;
     std::vector<double> par(m->par());
-    par.at(0) = -3.0;
-    par.at(1) = 1.0;
-    par.at(2) = 0.1;
+    par.at(0) = -4.5;
+    par.at(1) = 4.0;
+    par.at(2) = 0.5;
     par.at(3) = -3.0;
     par.at(4) = -2.0;
     m->par(par);
@@ -115,6 +152,7 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
 
     const uint32_t num_points = 10;
     runner(&s, &a, num_points, 1.0);
+    std::cout << "count: " << s.state().inf_bits.count() << std::endl;
 
     std::vector<Transition<EbolaState>> history(
             Transition<EbolaState>::from_sequence(s.history(), s.state()));
@@ -144,6 +182,8 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
         double abserr;
         gsl_deriv_central(&F, par.at(i), 1e-3, &result, &abserr);
 
+        std::cout << i << ": " << grad_val.at(i) << std::endl;
+
         EXPECT_NEAR(grad_val.at(i), result, eps)
             << "gradient failed for parameter " << i;
     }
@@ -151,7 +191,44 @@ TEST(TestEbolaStateGravityModel,TestLLGradient) {
 
 
 TEST(TestEbolaStateGravityModel,TestLLHessian) {
-    EbolaData::init();
+    njm::tools::Rng rng;
+    {
+        // set up mock ebola network from a grid
+        NetworkInit init;
+        init.set_type(NetworkInit_NetType_GRID);
+        init.set_dim_x(10);
+        init.set_dim_y(10);
+        init.set_wrap(false);
+
+        std::shared_ptr<Network> grid = Network::gen_network(init);
+
+        std::vector<std::string> str_var(grid->size(), "blank");
+
+        std::vector<uint32_t> region(grid->size());
+        std::generate(region.begin(), region.end(),
+                [&rng] () {return (uint32_t)rng.rint(0, 10);});
+
+        std::vector<int> outbreaks(grid->size());
+        std::generate(outbreaks.begin(), outbreaks.end(),
+                [&rng] () {return rng.rint(0, 100);});
+
+        std::vector<double> population(grid->size());
+        std::generate(population.begin(), population.end(),
+                [&rng] () {return rng.runif(3.0, 5.0);});
+
+        std::vector<double> x(grid->size());
+        std::vector<double> y(grid->size());
+
+        for (uint32_t i = 0; i < grid->size(); ++i) {
+            x.at(i) = grid->get_node(i).x();
+            y.at(i) = grid->get_node(i).y();
+        }
+
+        EbolaData::deinit();
+        EbolaData::init(str_var, str_var, str_var, region,
+                outbreaks, population, x, y);
+    }
+
     // generate network
     NetworkInit init;
     init.set_type(NetworkInit_NetType_EBOLA);
@@ -163,11 +240,10 @@ TEST(TestEbolaStateGravityModel,TestLLHessian) {
             new EbolaStateGravityModel(n));
 
     // set par
-    njm::tools::Rng rng;
     std::vector<double> par(m->par());
-    par.at(0) = -3.0;
-    par.at(1) = 1.0;
-    par.at(2) = 0.1;
+    par.at(0) = -4.5;
+    par.at(1) = 4.0;
+    par.at(2) = 0.5;
     par.at(3) = -3.0;
     par.at(4) = -2.0;
     m->par(par);
@@ -222,59 +298,96 @@ TEST(TestEbolaStateGravityModel,TestLLHessian) {
 }
 
 
-TEST(TestEbolaStateGravityModel, EstPar) {
-    EbolaData::init();
-    NetworkInit init;
-    init.set_type(NetworkInit_NetType_EBOLA);
+// TEST(TestEbolaStateGravityModel, EstPar) {
+//     njm::tools::Rng rng;
+//     {
+//         // set up mock ebola network from a grid
+//         NetworkInit init;
+//         init.set_type(NetworkInit_NetType_GRID);
+//         init.set_dim_x(10);
+//         init.set_dim_y(10);
+//         init.set_wrap(false);
 
-    const std::shared_ptr<Network> n = Network::gen_network(init);
+//         std::shared_ptr<Network> grid = Network::gen_network(init);
 
-    const std::shared_ptr<EbolaStateGravityModel> m(
-            new EbolaStateGravityModel(n));
+//         std::vector<std::string> str_var(grid->size(), "blank");
 
-    njm::tools::Rng rng;
-    std::vector<double> par(m->par_size());
-    par.at(0) = -3.0;
-    par.at(1) = 1.0;
-    par.at(2) = 0.08;
-    par.at(3) = -1.0;
-    par.at(4) = -1.5;
-    m->par(par);
+//         std::vector<uint32_t> region(grid->size());
+//         std::generate(region.begin(), region.end(),
+//                 [&rng] () {return (uint32_t)rng.rint(0, 10);});
 
-    System<EbolaState> s(n,m);
-    s.start();
+//         std::vector<int> outbreaks(grid->size());
+//         std::generate(outbreaks.begin(), outbreaks.end(),
+//                 [&rng] () {return rng.rint(0, 100);});
 
-    const std::shared_ptr<ProximalAgent<EbolaState> > pa(
-            new ProximalAgent<EbolaState>(n));
-    const std::shared_ptr<RandomAgent<EbolaState> > ra(
-            new RandomAgent<EbolaState>(n));
-    EpsAgent<EbolaState> ea(n, pa, ra, 0.1);
+//         std::vector<double> population(grid->size());
+//         std::generate(population.begin(), population.end(),
+//                 [&rng] () {return rng.runif(3.0, 5.0);});
 
-    const uint32_t num_points = 10;
-    runner(&s, &ea, num_points, 1.0);
+//         std::vector<double> x(grid->size());
+//         std::vector<double> y(grid->size());
 
-    std::vector<Transition<EbolaState> > history(
-            Transition<EbolaState>::from_sequence(s.history(), s.state()));
-    ASSERT_EQ(history.size(), num_points);
+//         for (uint32_t i = 0; i < grid->size(); ++i) {
+//             x.at(i) = grid->get_node(i).x();
+//             y.at(i) = grid->get_node(i).y();
+//         }
 
-    // scale paramters
-    std::vector<double> start_par = par;
-    std::for_each(start_par.begin(), start_par.end(),
-            [&rng] (double & x) {
-                x += rng.rnorm(0.0, std::abs(x) / 10.0);
-            });
-    m->par(start_par);
+//         EbolaData::deinit();
+//         EbolaData::init(str_var, str_var, str_var, region,
+//                 outbreaks, population, x, y);
+//     }
 
-    m->est_par(history);
+//     // network
+//     NetworkInit init;
+//     init.set_type(NetworkInit_NetType_EBOLA);
 
-    const std::vector<double> est_par = m->par();
-    for (uint32_t i = 0; i < m->par_size(); ++i) {
-        const double diff(par.at(i) - est_par.at(i));
-        EXPECT_LT(std::abs(diff / par.at(i)), 0.1)
-            << "Par " << i << " failed with truth " << par.at(i)
-            << " and estimate " << est_par.at(i);
-    }
-}
+//     const std::shared_ptr<Network> n = Network::gen_network(init);
+
+//     const std::shared_ptr<EbolaStateGravityModel> m(
+//             new EbolaStateGravityModel(n));
+
+//     std::vector<double> par(m->par_size());
+//     par.at(0) = -4.5;
+//     par.at(1) = 4.0;
+//     par.at(2) = 0.5;
+//     par.at(3) = -3.0;
+//     par.at(4) = -2.0;
+//     m->par(par);
+
+//     System<EbolaState> s(n,m);
+//     s.start();
+
+//     const std::shared_ptr<ProximalAgent<EbolaState> > pa(
+//             new ProximalAgent<EbolaState>(n));
+//     const std::shared_ptr<RandomAgent<EbolaState> > ra(
+//             new RandomAgent<EbolaState>(n));
+//     EpsAgent<EbolaState> ea(n, pa, ra, 0.1);
+
+//     const uint32_t num_points = 100;
+//     runner(&s, &ea, num_points, 1.0);
+
+//     std::vector<Transition<EbolaState> > history(
+//             Transition<EbolaState>::from_sequence(s.history(), s.state()));
+//     ASSERT_EQ(history.size(), num_points);
+
+//     // scale paramters
+//     std::vector<double> start_par = par;
+//     std::for_each(start_par.begin(), start_par.end(),
+//             [&rng] (double & x) {
+//                 x += rng.rnorm(0.0, std::abs(x) / 10.0);
+//             });
+//     m->par(start_par);
+
+//     m->est_par(history);
+
+//     const std::vector<double> est_par = m->par();
+//     for (uint32_t i = 0; i < m->par_size(); ++i) {
+//         const double diff(par.at(i) - est_par.at(i));
+//         EXPECT_LT(std::abs(diff / par.at(i)), 0.1)
+//             << "Par " << i << " failed with truth " << par.at(i)
+//             << " and estimate " << est_par.at(i);
+//     }
+// }
 
 
 
