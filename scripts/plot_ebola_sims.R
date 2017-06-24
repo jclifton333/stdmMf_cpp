ef528efc5fb8e019a7dc9b8a9a8ca76e772cdd07
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-06-24 14:10:50 -0400

    use final infection propertion for results

--- scripts/plot_ebola_sims.R
+++ scripts/plot_ebola_sims.R
@@ -4,14 +4,31 @@ library(xtable)
 library(readr)
 library(reshape2)
 
-filename = "../data/2017-06-21_13-36-31/ebola_Gravity-Gravity_raw.txt"
-raw_data = read.csv(filename)
+filename = "../data/2017-06-21_13-36-31/ebola_Gravity-Gravity_history.txt"
+raw_data = read_delim(filename, ",", trim_ws = TRUE,
+                      col_types = cols(col_character(), ## agent
+                                       col_integer(), ## rep
+                                       col_integer(), ## time
+                                       col_integer(), ## node
+                                       col_integer(), ## inf
+                                       col_integer() ## trt
+                                       ),
+                      progress = FALSE)
+
+se = function(x) sqrt(var(x)/length(x))
+
+agg_data = raw_data[which(raw_data$time == max(raw_data$time)),]
+agg_data = summarise(group_by(agg_data, agent),
+                     value_mean = mean(inf),
+                     value_sse = se(inf))
+
+agg_data$network = "Ebola"
 
 
-proc_data = raw_data
-proc_data$value_mean_ssd = paste(round(raw_data$value_mean, 2), " (",
-                                 round(raw_data$value_ssd, 2), ")", sep="")
-proc_data = dcast(proc_data, network ~ agent, value.var = "value_mean_ssd")
+proc_data = agg_data
+proc_data$value_mean_sse = paste(round(agg_data$value_mean, 4), " (",
+                                 round(agg_data$value_sse, 4), ")", sep="")
+proc_data = dcast(proc_data, network ~ agent, value.var = "value_mean_sse")
 proc_data$network = NULL
 proc_data = proc_data[,c("none", "random", "proximal", "myopic", "vfn")]
 names(proc_data) = c("None", "Random", "Proximal", "Myopic", "Model based")
--- scripts/plot_toy_sims.R
+++ scripts/plot_toy_sims.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(readr)
 library(viridis)
 library(plyr)
+library(dplyr)
+library(progress)
 
 data_dir = "../data/2017-05-28_16-13-13"
 
@@ -19,13 +21,20 @@ nets = data.frame(
                    "random-100", "random-500", "random-1000")
        )
 
-mods_miss = data.frame(
+mods_miss <- data.frame(
               miss_prop = c(0.0, 0.25, 0.5, 0.75, 1.0),
               mod_system = c("NoImNoSo", "Mixture-NoImNoSo-75-PosImNoSo-25",
-                          "Mixture-NoImNoSo-50-PosImNoSo-50",
-                          "Mixture-NoImNoSo-25-PosImNoSo-75", "PosImNoSo"),
+                             "Mixture-NoImNoSo-50-PosImNoSo-50",
+                             "Mixture-NoImNoSo-25-PosImNoSo-75", "PosImNoSo"),
               mod_agent = c("NoImNoSo", "NoImNoSo", "NoImNoSo", "NoImNoSo", "NoImNoSo")
-         )
+             )
+
+se = function(x) sqrt(var(x)/length(x))
+
+
+pb = progress_bar$new(total = nrow(nets) * nrow(mods_miss),
+                      format = "loading data [:bar]",
+                      clear = TRUE, width = 60)
 
 sim_data = NULL
 for (net_index in 1:nrow(nets)) {
@@ -35,23 +44,33 @@ for (net_index in 1:nrow(nets)) {
                       nets$label[net_index], "_",
                       mods_miss$mod_system[mod_index], "-",
                       mods_miss$mod_agent[mod_index], "_",
-                      "raw.txt", sep="")
+                      "history.txt", sep="")
 
-    raw_data = read_delim(file_name, ",",
-                          col_types = c(col_character(), ## network
-                                        col_character(), ## model
-                                        col_character(), ## agent
-                                        col_double(), ## value_mean
-                                        col_double(), ## value_ssd
-                                        col_double() ## time_mean
-                                        ))
+    raw_data = read_delim(file_name, ",", trim_ws = TRUE,
+                          col_types = cols(col_character(), ## agent
+                                           col_integer(), ## rep
+                                           col_integer(), ## time
+                                           col_integer(), ## node
+                                           col_integer(), ## inf
+                                           col_double(), ## shield
+                                           col_integer() ## trt
+                                           ),
+                          progress = FALSE)
+
+    ## isolate last time point
+    agg_data = raw_data[which(raw_data$time == max(raw_data$time)),]
+    agg_data = summarise(group_by(agg_data, agent),
+                         value_mean = mean(inf),
+                         value_sse = se(inf))
 
     ## add data for plotting
-    raw_data$network_type = nets$type[net_index]
-    raw_data$network_size = nets$size[net_index]
-    raw_data$miss_prop = mods_miss$miss_prop[mod_index]
+    agg_data$network_type = nets$type[net_index]
+    agg_data$network_size = nets$size[net_index]
+    agg_data$miss_prop = mods_miss$miss_prop[mod_index]
 
-    sim_data = rbind(sim_data, raw_data)
+    sim_data = rbind(sim_data, agg_data)
+
+    pb$tick()
   }
 }
 
@@ -67,16 +86,25 @@ sim_data$network_type = mapvalues(sim_data$network_type,
 
 p = ggplot(data = sim_data)
 p = p + geom_line(aes(x = miss_prop, y = value_mean,
-                      color = agent), size = 1.5)
+                      color = agent, lty = agent), size = 1.0)
 p = p + facet_grid(network_type ~ network_size)
-p = p + scale_color_manual("Treatment Strategy",
+p = p + scale_color_viridis("Treatment Strategy",
                            breaks = c("No treatment", "Proximal", "Random", "Myopic",
                                       "Model based", "Model free"),
-                           values = c("#E69F00", "#009E73", "#F0E442",
-                                      "#0072B2", "#D55E00", "#CC79A7"))
+                           discrete = TRUE)
+p = p + scale_linetype_manual("Treatment Strategy",
+                              breaks = c("No treatment", "Proximal", "Random", "Myopic",
+                                         "Model based", "Model free"),
+                              values = c("No treatment" = 4,
+                                         "Proximal" = 4,
+                                         "Random" = 4,
+                                         "Myopic" = 4,
+                                         "Model based" = 1,
+                                         "Model free" = 1))
 p = p + ylab("Estimated mean value")
 p = p + xlab("Misspecification proportion")
-p = p + theme(panel.spacing = unit(1.5, "lines"))
+p = p + theme(panel.spacing = unit(1.5, "lines"),
+              legend.key.width=unit(3,"line"))
 print(p)
 
 ggsave("../data/figures/toy_sim_results.pdf", p)
