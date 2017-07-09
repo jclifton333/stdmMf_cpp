93113a4748e501f62e98e1cdc8850f1dd21a416c
Nicholas J Meyer <nick.j.meyer@gmail.com>
2017-07-09 09:49:09 -0400

    disable thompson sampling

--- src/main/vfnMaxSimPerturbAgent.cpp
+++ src/main/vfnMaxSimPerturbAgent.cpp
@@ -111,52 +111,52 @@ std::vector<double> VfnMaxSimPerturbAgent<State>::train(
 
     this->model_->est_par(history);
 
-    // get information matrix and take inverse sqrt
-    std::vector<double> hess = this->model_->ll_hess(history);
-    njm::linalg::mult_b_to_a(hess, -1.0 * history.size());
+    // // get information matrix and take inverse sqrt
+    // std::vector<double> hess = this->model_->ll_hess(history);
+    // njm::linalg::mult_b_to_a(hess, -1.0 * history.size());
 
-    const arma::mat hess_mat(hess.data(), this->model_->par_size(),
-            this->model_->par_size());
-    arma::mat eigvec;
-    arma::vec eigval;
-    arma::eig_sym(eigval, eigvec, hess_mat);
-    for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
-        if (eigval(i) > 0.1)
-            eigval(i) = std::sqrt(1.0 / eigval(i));
-        else
-            eigval(i) = 0.0;
-    }
-    // threshold eigen vectors
-    for (auto it = eigvec.begin(); it != eigvec.end(); ++it) {
-        if (std::abs(*it) < 1e-3) {
-            *it = 0.0;
-        }
-    }
-    arma::mat var_sqrt = eigvec * arma::diagmat(eigval) * eigvec.t();
-    // threshold sqrt matrix
-    for (auto it = var_sqrt.begin(); it != var_sqrt.end(); ++it) {
-        if (*it < 1e-3) {
-            *it = 0.0;
-        }
-    }
+    // const arma::mat hess_mat(hess.data(), this->model_->par_size(),
+    //         this->model_->par_size());
+    // arma::mat eigvec;
+    // arma::vec eigval;
+    // arma::eig_sym(eigval, eigvec, hess_mat);
+    // for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
+    //     if (eigval(i) > 0.1)
+    //         eigval(i) = std::sqrt(1.0 / eigval(i));
+    //     else
+    //         eigval(i) = 0.0;
+    // }
+    // // threshold eigen vectors
+    // for (auto it = eigvec.begin(); it != eigvec.end(); ++it) {
+    //     if (std::abs(*it) < 1e-3) {
+    //         *it = 0.0;
+    //     }
+    // }
+    // arma::mat var_sqrt = eigvec * arma::diagmat(eigval) * eigvec.t();
+    // // threshold sqrt matrix
+    // for (auto it = var_sqrt.begin(); it != var_sqrt.end(); ++it) {
+    //     if (*it < 1e-3) {
+    //         *it = 0.0;
+    //     }
+    // }
 
-    // sample new parameters
-    arma::vec std_norm(this->model_->par_size());
-    for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
-        std_norm(i) = this->rng_->rnorm_01();
-    }
-    const std::vector<double> par_samp(
-            njm::linalg::add_a_and_b(this->model_->par(),
-                    arma::conv_to<std::vector<double> >::from(
-                            var_sqrt * std_norm)));
-    // check for finite values
-    std::for_each(par_samp.begin(), par_samp.end(),
-            [] (const double & x_) {
-                LOG_IF(FATAL, !std::isfinite(x_));
-            });
+    // // sample new parameters
+    // arma::vec std_norm(this->model_->par_size());
+    // for (uint32_t i = 0; i < this->model_->par_size(); ++i) {
+    //     std_norm(i) = this->rng_->rnorm_01();
+    // }
+    // const std::vector<double> par_samp(
+    //         njm::linalg::add_a_and_b(this->model_->par(),
+    //                 arma::conv_to<std::vector<double> >::from(
+    //                         var_sqrt * std_norm)));
+    // // check for finite values
+    // std::for_each(par_samp.begin(), par_samp.end(),
+    //         [] (const double & x_) {
+    //             LOG_IF(FATAL, !std::isfinite(x_));
+    //         });
 
-    // set new parameters
-    this->model_->par(par_samp);
+    // // set new parameters
+    // this->model_->par(par_samp);
 
     CHECK_GT(this->final_t_, history.size());
     const uint32_t num_points = this->final_t_ - history.size();
